7e0bcf06196b9319a33c22fe6bb5da741eb97056
Paweł Hejlyk <110741461+PavloKheylyk@users.noreply.github.com>
2024-06-19 14:15:07 +0200

    update1

--- quiz.go
+++ quiz.go
@@ -21,9 +21,25 @@ func main() {
 		return
 	}
 
+	score := 0
+
 	fmt.Printf("What is better, AMD or Intel? ")
 	var answer string
 	fmt.Scan(&answer)
 
-	fmt.Println(answer)
+	if answer == "Intel" {
+		fmt.Println("Correct!")
+	} else {
+		fmt.Println("Incorrect")
+
+	fmt.Printf("How many cores does Ryzen 9 3900x have? ")
+	var cores int
+	fmt.Scan(&cores)
+
+	if cores == 12 {
+		fmt.Println("Correct!")
+	} else {
+		fmt.Println("Incorrect")
+		//return
+	}
 }
