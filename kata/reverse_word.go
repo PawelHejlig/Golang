31609535b58ef9e08d8708b2103d2bb9903855bc
Paweł Hejlyk <110741461+PavloKheylyk@users.noreply.github.com>
2024-07-01 22:00:51 +0200

    reverse

--- kata/reverse_word.go
+++ kata/reverse_word.go
@@ -1,11 +1,21 @@
 package main
 
-//"fmt"
+import "fmt"
+
 //s "strings"
 
 func Solution(word string) string {
+	runes := []rune(word)
+	n := len(runes)
+	fmt.Println(word, runes)
+	// Reverse the slice of runes
+	for i := 0; i < n/2; i++ {
+		fmt.Println(i, n)
+		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
+	}
+	fmt.Println(runes)
+	return string(runes)
 
-	return ""
 }
 
 func main() {
