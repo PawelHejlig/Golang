85d0b5eb77c2aa2930fbe6a3a16ce0ff75a6e9db
Paweł Hejlyk <110741461+PavloKheylyk@users.noreply.github.com>
2024-07-02 22:30:52 +0200

    .

--- kata/expression_matters_kata.go
+++ kata/expression_matters_kata.go
@@ -2,18 +2,24 @@ package main
 
 import "fmt"
 
-//import "fmt"
+/*import "sort"
 
-//s "strings"
 
 func ExpressionMatter(a int, b int, c int) int {
-	var x1, x2, x3, x4 int
-	x1 = (a * (b + c))
-	x2 = (a * b * c)
-	x3 = (a + b*c)
-	x4 = ((a + b) * c)
+    arr := []int{a*(b+c), a*b*c, a+b+c, (a+b)*c}
+    sort.Ints(arr)
+    return arr[len(arr)-1]
+}
+*/
 
-	numbers := []int{x1, x2, x3, x4}
+func ExpressionMatter(a int, b int, c int) int {
+	x1 := (a * (b + c))
+	x2 := (a * b * c)
+	x3 := (a + b*c)
+	x4 := ((a + b) * c)
+	x5 := (a + b + c)
+
+	numbers := []int{x1, x2, x3, x4, x5}
 	max := numbers[0]
 	for _, num := range numbers[1:] {
 		if num > max {
